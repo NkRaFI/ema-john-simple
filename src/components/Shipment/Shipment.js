b7c6968fd2c29b3ee56b757752b73f72759b253c
NkRaFI <nkrafi11@gmail.com>
2021-04-01 22:12:14 +0600

    heroku server and mongodb database added

--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -10,7 +10,7 @@ const Shipment = () => {
     const onSubmit = data => {
         const savedCart = getDatabaseCart();
         const orderInfo = {...loggedInUser, products: savedCart, shipment: data, orderTime: new Date()}
-        fetch('http://localhost:5000/addOrder', {
+        fetch('https://pumpkin-tart-01673.herokuapp.com/addOrder', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
